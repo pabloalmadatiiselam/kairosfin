4196c3587b6200ffd4be4e64a8b94e304a2dc335
Pablo Almada <pabje@hotmail.com>
2026-02-02 17:36:08 -0300

    Corregir imports para Render

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /opt/render/project/src/src
-python -m uvicorn main:app --host 0.0.0.0 --port $PORT
+cd /opt/render/project/src
+python -m uvicorn src.main:app --host 0.0.0.0 --port $PORT
